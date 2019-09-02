9f67a2f86a9d392e0e108e97365bdff0b9b304ce
hathach <thach@tinyusb.org>
2019-09-02 15:52:41 +0700

    adding lpc55xx support

---
+++ lpc55xx/devices/LPC55S69/drivers/fsl_power.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016, Freescale Semiconductor, Inc.
+ * Copyright 2016, NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <mcu/nxp/lpc_driver/lpc55xx/devices/LPC55S69/drivers/fsl_common.h>
+#include <mcu/nxp/lpc_driver/lpc55xx/devices/LPC55S69/drivers/fsl_power.h>
+/* Component ID definition, used by tools. */
+#ifndef FSL_COMPONENT_ID
+#define FSL_COMPONENT_ID "platform.drivers.power"
+#endif
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+/* Empty file since implementation is in header file and power library */
